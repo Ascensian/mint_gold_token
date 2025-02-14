5931e4ba43292886fcb41fcedb98d19d0eee998d
Ascensian <my.hugogerard@protonmail.com>
2025-02-14 11:29:40 +0100

    add remapping

--- src/GoldToken.sol
+++ src/GoldToken.sol
@@ -1,4 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract GoldToken {}
